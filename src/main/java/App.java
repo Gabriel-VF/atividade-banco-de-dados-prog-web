cfdf2490db54828c873d0772c1d29ec3eb1a80db
Gabriel-VF <vergabriel07@gmail.com>
2026-06-23 04:50:25 -0300

    :recycle: refactor: Altera main() para utilizar DAOFactory

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,17 @@
 import database.*;
 
-import java.sql.Connection;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-import database.daoimpl.*;
+import database.dao.*;
 import database.entities.*;
 
 public class App {
   public static void main(String[] args) throws Exception {
     ConnectionFactory.setUser("");
     ConnectionFactory.setPassword("");
-    Connection connection = ConnectionFactory.getConnection();
 
-    DepartmentDAOImpl departmentDAOImpl = new DepartmentDAOImpl(connection);
+    DepartmentDAO departmentDAO = DAOFactory.createDepartmentDAO();
 
 
 
@@ -30,7 +28,7 @@ public class App {
 
     // Testando findAll() para departamento
     System.out.println("\n> [Testando findAll() para departamento]");
-    ArrayList<Department> departmentList = new ArrayList<>(departmentDAOImpl.findAll());
+    ArrayList<Department> departmentList = new ArrayList<>(departmentDAO.findAll());
     for (int i = 0; i < departmentList.size(); i++) {
       System.out.printf("Departamento %s: %s\n", departmentList.get(i).getId(), departmentList.get(i).getName());
     }
@@ -38,25 +36,25 @@ public class App {
     // Testando findById() para departamento
     System.out.println("\n> [Testando findById() para departamento]");
     Department dp1 = new Department();
-    dp1 = departmentDAOImpl.findById(1);
+    dp1 = departmentDAO.findById(1);
     System.out.printf("Departamento %s: %s\n", dp1.getId(), dp1.getName());
 
     // Testando delete() para departamento não vai funcionar se vendedor é dependência de outro registro
     System.out.println("\n> [Testando delete() para departamento. Não irá funcionar se departamento é dependência de outro registro]");
-    departmentDAOImpl.deleteById(13);
+    departmentDAO.deleteById(13);
 
     // Testando insert() para departamento
     System.out.println("\n> [Testando insert() para departamento]");
-    String dpNovoNome = "Novo departamento de número " + (departmentDAOImpl.findAll().size() + 1);
+    String dpNovoNome = "Novo departamento de número " + (departmentDAO.findAll().size() + 1);
     System.out.println("\n> [Inserindo departamento: " + dpNovoNome + "]");
     Department dpInsert = new Department(0, dpNovoNome);
-    departmentDAOImpl.insert(dpInsert);
+    departmentDAO.insert(dpInsert);
 
     // Testando update() para departamento
     System.out.println("\n> [Testando update() para departamento]");
-    System.out.printf("\n> [Fazendo update de departamento com id %s se existe]\n", departmentDAOImpl.findAll().size());
-    Department departamentoNovo = new Department(departmentDAOImpl.findAll().size(), "Novo nome");
-    departmentDAOImpl.update(departamentoNovo);
+    System.out.printf("\n> [Fazendo update de departamento com id %s se existe]\n", departmentDAO.findAll().size());
+    Department departamentoNovo = new Department(departmentDAO.findAll().size(), "Novo nome");
+    departmentDAO.update(departamentoNovo);
 
 
 
@@ -69,11 +67,11 @@ public class App {
 
 
     System.out.println("\n\n---------- [INICIANDO TESTES PARA VENDEDOR] ----------\n");
-    SellerDAOImpl sellerDAOImpl = new SellerDAOImpl(connection);
+    SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 
     // Testando findAll() para vendedor
     System.out.println("\n> [Testando findAll() para vendedor]");
-    ArrayList<Seller> sellerList = new ArrayList<>(sellerDAOImpl.findAll());
+    ArrayList<Seller> sellerList = new ArrayList<>(sellerDAO.findAll());
     for (int i = 0; i < sellerList.size(); i++) {
       System.out.printf("Vendedor %s: %s\n", sellerList.get(i).getId(), sellerList.get(i).getEmail());
     }
@@ -81,25 +79,25 @@ public class App {
     // Testando findById() para vendedor
     System.out.println("\n> [Testando findById() para vendedor]");
     Seller s1 = new Seller();
-    s1 = sellerDAOImpl.findById(1);
+    s1 = sellerDAO.findById(1);
     System.out.printf("Vendedor %s: %s\n", s1.getId(), s1.getEmail());
 
     // Testando delete() para vendedor. Não irá funcionar se vendedor é dependência de outro registro
     System.out.println("\n> [Testando delete() para vendedor. Não irá funcionar se vendedor é dependência de outro registro]");
-    sellerDAOImpl.deleteById(10);
+    sellerDAO.deleteById(10);
 
     // Testando insert() para departamento
     System.out.println("\n> [Testando insert() para vendedor]");
-    String emailInsert = "novo.seller." + (sellerDAOImpl.findAll().size() + 1) + "@gmail.com";
+    String emailInsert = "novo.seller." + (sellerDAO.findAll().size() + 1) + "@gmail.com";
     System.out.println("\n> [Inserindo vendedor de email: " + emailInsert + "]");
-    Seller sellerInsert = new Seller(0, "novo seller", emailInsert, LocalDate.now(), 1901.19, departmentDAOImpl.findById(1));
-    sellerDAOImpl.insert(sellerInsert);
+    Seller sellerInsert = new Seller(0, "novo seller", emailInsert, LocalDate.now(), 1901.19, departmentDAO.findById(1));
+    sellerDAO.insert(sellerInsert);
 
     // Testando update() para vendedor
     System.out.println("\n> [Testando update() para vendedor]");
-    System.out.printf("\n> [Fazendo update de vendedor com id %s se existe]\n", sellerDAOImpl.findAll().size());
-    Seller sellerUpdate = new Seller(sellerDAOImpl.findAll().size(), "seller com update", emailInsert, LocalDate.now(), 1635.9, departmentDAOImpl.findById(1));
-    sellerDAOImpl.update(sellerUpdate);
+    System.out.printf("\n> [Fazendo update de vendedor com id %s se existe]\n", sellerDAO.findAll().size());
+    Seller sellerUpdate = new Seller(sellerDAO.findAll().size(), "seller com update", emailInsert, LocalDate.now(), 1635.9, departmentDAO.findById(1));
+    sellerDAO.update(sellerUpdate);
 
     ConnectionFactory.getInstance().closeConnection();
 
