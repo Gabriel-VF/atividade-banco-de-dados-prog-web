9b8659ab5b7d90ddbdd13ac73ddac33726985e9d
Gabriel-VF <vergabriel07@gmail.com>
2026-06-23 04:38:42 -0300

    :sparkles: feat: Adiciona código para DAOFactory

--- src/main/java/database/DAOFactory.java
+++ src/main/java/database/DAOFactory.java
@@ -1,5 +1,14 @@
 package database;
 
-public class DAOFactory {
+import database.dao.*;
+import database.daoimpl.*;
 
+public class DAOFactory {
+  public static DepartmentDAO createDepartmentDAO() {
+      return new DepartmentDAOImpl(ConnectionFactory.getConnection());
+  }
+
+  public static SellerDAO createSellerDAO() {
+      return new SellerDAOImpl(ConnectionFactory.getConnection());
+  }
 }
